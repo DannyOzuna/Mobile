cc9db046ef28c302128791114a8770841b6b42f3
DannyOzuna <danny20011010@gmail.com>
2021-10-14 07:31:09 -0400

    Agregue los screens

---
+++ Primer_Parcial/screens/Navigation.js
@@ -0,0 +1,10 @@
+import React from "react";
+
+//Navigation Components
+
+import { NavigationContainer } from "@react-navigation/native";
+import { createStackNavigator } from "@react-navigation/stack";
+import { createBottomTabNavigator } from "@react-navigation/bottom-tabs";
+import IonicIcon from "react-native-vector-icons/Ionicons";
+
+//Imports Screens
---
+++ Primer_Parcial/screens/Profile.js
@@ -0,0 +1,14 @@
+import React from "react";
+import { SafeAreaView, Text } from "react-native";
+
+const Profile = (props) => {
+  return (
+    <SafeAreaView
+      style={{ flex: 1, justifyContent: "center", alignItems: "center" }}
+    >
+      <Text>Profile Screen</Text>
+    </SafeAreaView>
+  );
+};
+
+export default Profile;
