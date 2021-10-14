b93a8ed4ee2a27c67650cf29baf9f61781496e70
DannyOzuna <danny20011010@gmail.com>
2021-10-14 07:40:51 -0400

    Hice algunos cambios que no tiene importancia

--- Primer_Parcial/screens/Navigation.js
+++ Primer_Parcial/screens/Navigation.js
@@ -7,4 +7,20 @@ import { createStackNavigator } from "@react-navigation/stack";
 import { createBottomTabNavigator } from "@react-navigation/bottom-tabs";
 import IonicIcon from "react-native-vector-icons/Ionicons";
 
+import { Text, Dimensions } from "react-native";
+
 //Imports Screens
+import Home from "../screens/Home";
+import Prifle from "../screens/Profile";
+
+const fullScreenWidth = Dimensions.get("window").width;
+
+const Stack = createStackNavigator();
+
+function HomeStackScreen() {
+  return (
+    <Stack.Navigator>
+      <Stack.Screen name="Home" component={Home} />
+    </Stack.Navigator>
+  );
+}
